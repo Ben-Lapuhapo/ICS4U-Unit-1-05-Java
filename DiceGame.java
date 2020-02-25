e3645a4c91c3a8d74a1307ff732e60968d461814
Ubuntu <ubuntu@ip-172-31-82-152.ec2.internal>
2020-02-25 02:17:17 +0000

    Added Basic Dice Game for JAVA

---
+++ DiceGame.java
@@ -0,0 +1,50 @@
+/****************************************************************************
+ *
+ * Created by: Ben Lapuhapo
+ * Created on: Feb 24 2019
+ * This program is a dice guessing game
+ *
+ ****************************************************************************/
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class DiceGame {
+    public static void main(String[] args) {
+
+    Random rand = new Random();
+    Scanner UserInput = new Scanner(System.in);
+
+    // Variables
+
+    // This will give a range from 0 - 5
+    int RandomNumber = rand.nextInt(6);
+    int UserGuess;
+    int Counter;
+    
+    //Add one so that the range is from 1 - 6 
+    RandomNumber += 1;
+    Counter = 0;
+
+    // Asks the user what the number is
+    System.out.print("Guess the secret number? (1 - 6):");
+    UserGuess = UserInput.nextInt();
+
+    // Repeats this process as long as the answer is wrong
+    while (RandomNumber != UserGuess) {
+        // One more trial that you took is added to the total
+        Counter += 1;
+
+        System.out.print("Your Guess is incorrect! ");
+        
+        // Asks the user again  
+        System.out.print(" Guess the secret number? (1 - 6):");
+        UserGuess = UserInput.nextInt();
+        }
+
+    // One more trial that you took is added to the total
+    Counter += 1;
+    System.out.print("Congratulations!! You only took " + Counter + " Try.");
+
+    }
+}
